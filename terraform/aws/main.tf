0a3ac46bf7fcea83a2c3ce52a1577775ecff7876
Niall Thomson <nthomson@pivotal.io>
2019-10-02 00:36:27 -0700

    Fix locals names

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -1,6 +1,6 @@
 locals {
   opsman_password              = "${var.opsman_password == "" ? random_string.opsman_password.result : var.opsman_password}"
-  opsman_decryption_passphrase = "${var.opsman_decryption_passphrase == "" ? random_string.opsman_password.result : var.opsman_decryption_passphrase}"
+  opsman_decryption_passphrase = "${var.opsman_decryption_passphrase == "" ? random_string.opsman_decryption_passphrase.result : var.opsman_decryption_passphrase}"
 }
 
 resource "random_string" "opsman_password" {
