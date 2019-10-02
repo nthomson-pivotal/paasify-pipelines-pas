75a2321ce0327053f4dc2f688d6a20ad0051a774
Niall Thomson <nthomson@pivotal.io>
2019-10-02 01:17:08 -0700

    Try tempaltes directory for interpolate

--- tasks/interpolate.sh
+++ tasks/interpolate.sh
@@ -2,6 +2,6 @@
 
 set -eu
 mkdir -p interpolated-templates
-/bin/gomplate --input-dir paasify-pipelines-pas/templates \
+/bin/gomplate --input-dir templates \
     -d data=file://$(pwd)/terraform-state/metadata?type=application/json \
     --output-dir interpolated-templates --verbose
