3c0bdb5be784642688ce64d59efc976eb3fe2964
Niall Thomson <nthomson@pivotal.io>
2019-10-02 00:18:39 -0700

    Fix templates path

--- tasks/interpolate.sh
+++ tasks/interpolate.sh
@@ -2,6 +2,6 @@
 
 set -eu
 mkdir -p interpolated-templates
-/bin/gomplate --input-dir templates \
+/bin/gomplate --input-dir paasify-pipelines-pas/templates \
     -d data=file://$(pwd)/terraform-state/metadata?type=application/json \
     --output-dir interpolated-templates --verbose
